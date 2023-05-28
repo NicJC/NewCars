36542f6b7ed9107b04d5ad084e5823f369d943f6
“[Nic <[nicjcoxen@gmail.com]>
2023-05-28 22:55:17 +1200

    update

--- NewCar.R
+++ NewCar.R
@@ -15,6 +15,7 @@ cars <- read_csv("C:/Data/newcars.csv")
 
 write.csv(cars,"New_cars.csv",row.names=FALSE)
 
-ggplot(cars, aes(x = AnnualFuelCost , y = as.factor(Fuel), col = as.factor(Gearbox) )) + geom_point() + labs(title = "New cars: Fuel efficiency")
+ggplot(cars, aes(x = AnnualFuelCost , y = as.factor(Fuel), col = as.factor(Gearbox) )) + geom_point(size = 2) +  
+  theme(axis.text.x = element_text(color="steelblue",size=12, angle=90) )+ labs(title = "New cars: Fuel efficiency", color = "gearbox" , y = "Fuel type" , x = "Annual fuel cost")
 
-ggsave("matrixPlot.png", width = 6, height = 6)
+ggsave("New Cars.png", width = 10, height = 7)
